a6af515bc9c33ac7e850f2002dda92827dd08331
Daniel Ruvalcaba <danyrwits@gmail.com>
2019-06-19 18:26:22 -0500

    Add Employee

--- app/Controllers/Http/EmployeeController.js
+++ app/Controllers/Http/EmployeeController.js
@@ -1,21 +1,38 @@
 'use strict'
 
+const Employee = use('App/Models/Employee')
+
 class EmployeeController {
 
-    index({request, view}){
-        let employees = [
-            {
-                name: "Daniel Ruvalcaba",
-                position: "Agile Team Leader",
-                salary: 1000
-            },
-            {
-                name: "John Doe",
-                position: "Frontend",
-                salary: 3000
-            }
-        ]
+    async index({request, view}){
+
+        let employees = await Employee.all()
+        employees = employees.toJSON()
         return view.render("employees.index", {employees})
+    }
+
+    create({request, view}){
+
+        return view.render("employees.create")
+
+    }
+
+    async store({request, response}){
+
+        let employee = request._body
+        delete employee._csrf
+        await Employee.create(employee)
+        return response.route("employees.index")
+
+    }
+
+    async delete({params}){
+
+        const { id } = params
+        const employee = await Employee.find(id)
+
+        await employee.delete()
+
     }
 
 }
