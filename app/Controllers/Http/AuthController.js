d16232041f043eeb9e806d247b11fc6511800924
Daniel Ruvalcaba <danyrwits@gmail.com>
2019-06-24 18:00:50 -0500

    Add Employee Card

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -6,9 +6,11 @@ class IndexController {
         return view.render("auth.login")
     }
 
-    async login({auth,request,response}) {
+    async login({auth,request,response,session}) {
         let { nickname, password } = request.all()
         await auth.attempt(nickname, password)
+
+        session.flash({ message: `Bienvenido de nuevo, ${auth.user.name}` })
 
         return response.route('index')
     }
